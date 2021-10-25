76656a6feeda23bfb0621765ba0ec27a9fa64db6
manojkale123 <manojvkale1994@gmail.com>
2021-10-25 20:18:27 +0200

    First commit

--- encryption.py
+++ encryption.py
@@ -3,6 +3,7 @@ import string
 plain_text= "Hello world"
 shift =1
 
+#encryption of data
 alphabet=string.ascii_lowercase
 shifted =alphabet[shift:]+alphabet[:shift]
 table=str.maketrans(alphabet,shifted)
