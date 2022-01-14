cb8f11589630589ce6ceb5f7fb965d1737b71d4b
manojkale123 <43934029+manojkale123@users.noreply.github.com>
2022-01-14 22:46:02 +0200

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -3,6 +3,9 @@ is_hot =False
 if is_hot:
     print("It is hot day")
     print("Drink plenty of water")
+elif:
+    is_cool:
+        print("Wear something..")
 else:
     print("It's a cold day")
     print("Wear warm clothes")
